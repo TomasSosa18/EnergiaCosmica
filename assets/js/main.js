0929b9fb2e27d70b0dedc79e73fad748cf8aa0ad
TomasSosa18 <90276141+TomasSosa18@users.noreply.github.com>
2022-07-05 05:27:43 -0300

    vs

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,7 +127,7 @@
 
 		// Toggle.
 			$navPanelToggle = $(
-				'<a href="#navPanel" id="navPanelToggle">Menu</a>'
+				'<a href="#navPanel" id="navPanelToggle"><img id="bars-nav" src="./assets/js/bars-staggered-solid.svg"</a>'
 			)
 				.appendTo($wrapper);
 
