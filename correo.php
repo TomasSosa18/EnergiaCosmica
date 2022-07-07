4d049afc04e0d7d1e9debdd0653b0fc4c00edc2c
TomasSosa18 <90276141+TomasSosa18@users.noreply.github.com>
2022-07-07 02:38:18 -0300

    Add files via upload

--- correo.php
+++ correo.php
@@ -1,17 +1,9 @@
 <?php
-
-
-    if (isset ($_POST['enviar'])) {
-      if (!empty ($_POST['name']) && !empty ($_POST['message']) && !empty ($_POST['email']) && (!empty ($_POST['email'])){
-        $name = $_POST['name'];
-        $message = $_POST['message'];
-        $email = $_POST['email'];
-        $header = "De: Energía Cósmica" . "\r\n";
-        $header = "X-mailer : PHP/". phpversion();
-        $mail= @mail($email, $message, $header);
-        if ($email) {
-            echo "<h4>¡ Mail ENVIADO exitosamente</h4>"
-        }
-      }
-    }
+$destino = "tomasgeronimopaiva@gmail.com";
+$name = $_POST["name"];
+$mail = $_POST["email"];
+$mensaje = $_POST["message"]
+$contenido = "Nombre: " . $name . "\nCorreo: " $email . "\nMensaje: " . $message;
+email($destino, "Contacto", $contenido);
+header("Location:index.html");
 ?>
